e428281864ed38629cddbb1dcc675853a467accd
Azhar Mahmood <mywritingfrenzy@gmail.com>
2024-12-10 19:47:44 +0530

    working on draggable toolbar

--- src/app/pipelines/toolbar.tsx
+++ src/app/pipelines/toolbar.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nodeTypes as nodes } from './nodes';
 
 type Props = {};
 
 const Toolbar = (props: Props) => {
-	return <div className=''>Toolbar</div>;
+	const [nodeTypes, setNodeTypes] = useState(nodes);
+
+	const handleDragStart = (event: React.DragEvent, nodeType: string) => {
+		event.dataTransfer.setData('application/reactflow', nodeType);
+		event.dataTransfer.effectAllowed = 'move';
+	};
+
+	const handleDragEnd = (event: React.DragEvent) => {
+		// Optional: Add any cleanup or state reset logic here
+	};
+
+	return (
+		<div className='bg-white flex gap-4 z-10'>
+			{Object.keys(nodeTypes).map((node) => (
+				<div
+					key={node}
+					draggable
+					onDragStart={(e) => handleDragStart(e, node)}
+					onDragEnd={handleDragEnd}
+					className='cursor-grab hover:bg-gray-100 p-2 rounded'
+				>
+					{node.replace('-', ' ')}
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Toolbar;
